53f3a279dd179580bc5bbcbab2c0edda81e84c27
Luiz Omena <luizfilipeyep@gmail.com>
2024-03-27 14:47:06 -0300

    project configs

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import GlobalStyle from "./styles/global"
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-itemns: center;
+  gap: 10px;
+`
+
+
+function App() {
+  return (  
+    <>
+      <Container/>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
+      <GlobalStyle/>
+    </>
+  )
+}
+
+export default App
